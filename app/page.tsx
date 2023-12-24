21873e7d58a37a0dc9b27360d937281fd4fa1862
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-12-24 17:49:37 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
-export default function Home() {
+import { Article } from "./types"
+
+async function getArticles() {
+  const res = await fetch("http://localhost:3000/api/articles", {
+    cache: "no-store",
+  });
+
+  if(!res.ok) {
+    throw new Error("Failed to fetch articles")
+  }
+
+  const data = await res.json()
+  return data.articles as Article[]
+}
+
+export default async function Home() {
+  const articles = await getArticles()
   return (
     <div>
       <h1>新着記事</h1>
       <ul>
-        <li>記事1</li>
-        <li>記事2</li>
-        <li>記事3</li>
+        {articles.map((article)=>(
+          <li key={article.id}>{article.title}</li>
+        ))}
       </ul>
     </div>
   )
