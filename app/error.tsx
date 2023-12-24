c55cd5eb8d2c70eef8de09ae031922e33f48eef9
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-12-24 18:03:07 +0900

    add error page

---
+++ app/error.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect } from "react";
+import { Heading, Button } from "./common/components";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <Heading mb={4}>予期せぬエラーが発生しました。</Heading>
+      <Button onClick={() => reset()}>Try again</Button>
+    </div>
+  );
+}
